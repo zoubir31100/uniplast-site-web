c8c8ed9065ade7d50d8792981d64224ffe06325d
zoubir31100 <33982793+zoubir31100@users.noreply.github.com>
2026-03-23 18:43:17 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f0f9ff',
+          100: '#e0f2fe',
+          500: '#0ea5e9',
+          600: '#0284c7',
+          700: '#0369a1',
+          900: '#0c4a6e',
+        },
+        secondary: '#64748b',
+      },
+      fontFamily: {
+        sans: ['Cairo', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
